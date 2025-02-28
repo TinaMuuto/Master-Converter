db723c2764fd1e9db15a91841d47710f83ce6f5c
TinaMuuto <145022549+TinaMuuto@users.noreply.github.com>
2025-02-28 18:28:23 +0100

    Update app.py
    store bogstaver.i word fil

--- app.py
+++ app.py
@@ -89,12 +89,12 @@ def match_article_numbers(user_df, master_df, library_df):
     )
     merged_df.loc[unmatched, 'PRODUCT'] = library_fallback['PRODUCT']
     
-    # If still no match, adjust based on output type
-    merged_df['Masterdata Output'] = merged_df['Base Article No.'].fillna('') + " - " + merged_df['Variant'].fillna('')
+   # If still no match, adjust based on output type
+    merged_df['Masterdata Output'] = (merged_df['Base Article No.'].fillna('') + " - " + merged_df['Variant'].fillna('')).str.upper()
     merged_df['Word Output'] = merged_df.apply(
-        lambda row: f"{row['Quantity']} X {row['PRODUCT']} {' - ' + row['Variant'] if row['Variant'] not in ['', 'Light Option: Off'] else ''}"
+        lambda row: f"{row['Quantity']} X {row['PRODUCT']} {' - ' + row['Variant'] if row['Variant'] not in ['', 'LIGHT OPTION: OFF'] else ''}"
         if pd.notna(row['PRODUCT']) else
-        f"{row['Quantity']} X {row['Short text']} {' - ' + row['Variant'] if row['Variant'] not in ['', 'Light Option: Off'] else ''}", axis=1)
+        f"{row['Quantity']} X {row['Short text']} {' - ' + row['Variant'] if row['Variant'] not in ['', 'LIGHT OPTION: OFF'] else ''}", axis=1).str.upper()
     
     return merged_df[['Quantity', 'Article No.', 'PRODUCT', 'Masterdata Output', 'Word Output']]
 
